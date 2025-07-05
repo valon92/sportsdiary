795e403471933ab11d7f1de4dc4ff70812694fed
valon92 <121667444+valon92@users.noreply.github.com>
2025-07-05 17:41:04 +0200

    U shtua GenderFilter.vue me ikona dhe responsivitet; u integrua në app.vue.

---
+++ resources/js/Components/GenderFilter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="d-flex justify-content-center my-4">
+    <div class="btn-group" role="group" aria-label="Gender filter">
+      <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" :checked="selectedGender === 'men'" @change="selectGender('men')">
+      <label class="btn btn-outline-success" for="btnradio1">Men</label>
+
+      <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off" :checked="selectedGender === 'women'" @change="selectGender('women')">
+      <label class="btn btn-outline-success" for="btnradio2">Women</label>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const selectedGender = ref('men'); // Vlera fillestare 'men'
+
+// Deklarojmë ngjarjen që ky komponent do të dërgojë
+const emit = defineEmits(['update:gender']);
+
+const selectGender = (gender) => {
+  selectedGender.value = gender;
+  emit('update:gender', gender); // Dërgon vlerën e zgjedhur te prindi
+};
+</script>
+
+<style scoped>
+/* Asnjë stil specifik nuk është i nevojshëm, Bootstrap menaxhon pamjen e radio butonave */
+</style>
--- resources/js/app.vue
+++ resources/js/app.vue
@@ -1,20 +1,16 @@
 <template>
   <Navbar />
-  <main class="flex-grow-1">
-    <div class="container my-5" style="min-height: 50vh;">
-        <h1>Mirësevjen në Sports Diary!</h1>
-        <p>Kjo është faqja jote kryesore.</p>
-        <p>Përmbajtja këtu do të shtyjë footer-in poshtë. Nëse ka pak përmbajtje, footer-i do të qëndrojë në fund të ekranit.</p>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <p>Fundi i përmbajtjes.</p>
-    </div>
-  </main>
+  
+  <GenderFilter />
+
   <Footer />
+
 </template>
 
 <script setup>
 import Navbar from './Components/Navbar.vue';
 import Footer from './Components/Footer.vue';
+import GenderFilter from './Components/GenderFilter.vue';
 </script>
 
 <style scoped>
